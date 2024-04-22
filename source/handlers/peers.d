1d866ec319afcc0ac5dd3ed2c8f9e46a44dd7597
Aravinda Vishwanathapura <mail@aravindavk.in>
2024-04-22 15:21:11 +0530

    Peer delete API
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- source/handlers/peers.d
+++ source/handlers/peers.d
@@ -3,6 +3,7 @@ module glusterd_plus.handlers.peers;
 import vibe.vibe;
 
 import glusterd_plus.handlers.helpers;
+import glusterd_plus.glustercli.helpers;
 
 void addPeer(HTTPServerRequest req, HTTPServerResponse res)
 {
@@ -17,6 +18,14 @@ void listPeers(HTTPServerRequest req, HTTPServerResponse res)
 
 void deletePeer(HTTPServerRequest req, HTTPServerResponse res)
 {
-
+    auto peerAddress = req.params.get("address");
+    try {
+        _cli.deletePeer(peerAddress);
+        res.statusCode = 204;
+        res.writeJsonBody(null);
+    } catch(GlusterCommandException err) {
+        res.statusCode = 500;
+        res.writeJsonBody(["error": "Failed to delete the Peer", "message": err.msg]);
+    }
 }
 
