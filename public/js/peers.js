450e9ce28b2dd4632c5c6eaea0676cdee44616e9
Aravinda Vishwanathapura <mail@aravindavk.in>
2024-06-23 11:24:20 +0530

    Peers management UI improvements
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- public/js/peers.js
+++ public/js/peers.js
@@ -1,11 +1,28 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('data', () => ({
         peers: [],
+        peerAddress: "",
+        showAddPeer: false,
         init() {
             var self = this;
             fetch('/api/v1/peers') // api for the get request
                 .then(response => response.json())
                 .then(data => self.peers = data);
+        },
+        addPeer() {
+            fetch('/api/v1/peers', {
+                method: 'post',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({address: this.peerAddress})
+            })
+                .then(response => response.json());
+        },
+        removePeer(address) {
+            var yes = confirm(`Are you sure want to remove the Peer (${address}) from the Cluster?`);
+            if (!yes)
+                return;
+
+            
         }
     }))
 });
